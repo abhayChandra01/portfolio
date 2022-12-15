4327bac4d7da1e8fd5e7755f6ac32ee48beabcff
abhayChandra01 <abhayxelp@gmail.com>
2022-12-15 15:04:41 +0530

    typewriter effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,47 @@
 import React from "react";
 import "../pages/pages.scss";
+import Typewriter from "typewriter-effect"
 
 const Home = () => {
   return (
     <>
-     
-          <header>
-            <div className="overlay">
-              <div className="inner">
-                <h2 className="title">Hello</h2>
-                <p>
-                I am
-                <h4 className="name-font">
-                  Abhay Chandra.
-                </h4> 
-                I am a
-                <h4 className="profile-font"> Software Engineer</h4>
-                at 
-                <span className="company-font"><a href="https://www.xelpmoc.in" target="_blank" style={{textDecoration:'none'}}> Xelpmoc Design And Tech Ltd.</a></span>
-                
-                </p>
-                <button className="btn" onClick={()=>window.open("https://github.com/abhayChandra01", "_blank")}>Projects</button>
-              </div>
-            </div>
-          </header>
+
+      <header>
+        <div className="overlay">
+          <div className="inner">
+            <h2 className="title">Hello</h2>
+            <p>
+              I am
+              <h4 className="name-font">
+                Abhay Chandra.
+              </h4>
+              <Typewriter
+
+                  onInit={(typewriter) => {
+
+                    typewriter
+
+                      .typeString("Software Engineer")
+
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString("Full Stack Web Developer")
+                      .start();
+                  }}
+                />
+
+              I am a
+              <h4 className="profile-font">
+                Software Engineer
+              </h4>
+              at
+              <span className="company-font"><a href="https://www.xelpmoc.in" target="_blank" style={{ textDecoration: 'none' }}> Xelpmoc Design And Tech Ltd.</a></span>
+
+            </p>
+            <button className="btn" onClick={() => window.open("https://github.com/abhayChandra01", "_blank")}>Projects</button>
+          </div>
+        </div>
+      </header>
 
 
     </>
